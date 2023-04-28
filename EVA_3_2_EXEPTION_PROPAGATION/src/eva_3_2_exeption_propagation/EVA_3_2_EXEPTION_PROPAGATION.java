e284429fb2c94dacb83b6b066cda9d86bc168220
Dirceu <124701508+Dirceuu@users.noreply.github.com>
2023-04-27 21:54:30 -0600

    Add files via upload

---
+++ EVA_3_2_EXEPTION_PROPAGATION/src/eva_3_2_exeption_propagation/EVA_3_2_EXEPTION_PROPAGATION.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_3_2_exeption_propagation;
+
+/**
+ *
+ * @author Alan
+ */
+public class EVA_3_2_EXEPTION_PROPAGATION {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
