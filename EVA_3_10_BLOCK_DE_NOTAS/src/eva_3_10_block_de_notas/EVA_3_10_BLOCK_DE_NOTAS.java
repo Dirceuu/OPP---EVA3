979dbd49e8877264a4deda88776f92d98893babc
Dirceu <124701508+Dirceuu@users.noreply.github.com>
2023-05-29 21:53:33 -0600

    Add files via upload

---
+++ EVA_3_10_BLOCK_DE_NOTAS/src/eva_3_10_block_de_notas/EVA_3_10_BLOCK_DE_NOTAS.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_3_10_block_de_notas;
+
+/**
+ *
+ * @author Alan
+ */
+public class EVA_3_10_BLOCK_DE_NOTAS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
